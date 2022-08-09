d42564787df25fecb2765be1b1337c49d8ab6e45
Daniel - <daniel.ajax.1996@hotmail.com>
2022-08-09 17:17:06 +0200

    projectid verkeerd gezet

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "muqxfs",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      projectId: "muqxfs"
+      
     },
   },
 });
